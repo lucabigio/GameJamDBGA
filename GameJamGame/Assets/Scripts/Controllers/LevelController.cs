d3b9c1e7eb5887b6cec08abf76d5ecb7f37d6e06
Francesco Brischetto <francesco_brischetto@hotmail.it>
2022-10-16 17:27:18 +0200

    Auto stash before merge of "main" and "origin/main"
    Death Panel Added

--- GameJamGame/Assets/Scripts/Controllers/LevelController.cs
+++ GameJamGame/Assets/Scripts/Controllers/LevelController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using static System.Net.Mime.MediaTypeNames;
 
 public class LevelController : MonoSingleton<LevelController>
 {
@@ -14,11 +16,32 @@ public class LevelController : MonoSingleton<LevelController>
     int maxSize = 11;
     [SerializeField]
     int increaseEvery = 5;
+    public GameObject deathPanel;
     void Start()
     {
         StartLevel();
     }
 
+    public void Lose()
+    {
+        deathPanel.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        UnityEngine.Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        deathPanel.SetActive(false);
+        StartLevel();
+    }
     public void StartLevel()
     {
         ScoreController.Instance.setupTimerTime();
